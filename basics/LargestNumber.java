fe1726b3ed2e9527fdef39464870ff4ca05f512d
Bijan Murmu <bijanmurmu123@gmail.com>
2025-07-20 22:00:26 +0530

    largest number

---
+++ basics/LargestNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the count of numbers: ");
+        int n = sc.nextInt();
+        int largest = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int num = sc.nextInt();
+            if (num > largest) {
+                largest = num;
+            }
+        }
+        System.out.println("The largest number is: " + largest);
+    }
+}
